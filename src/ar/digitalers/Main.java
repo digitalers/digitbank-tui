617222e8ddf7c12672663731e1c219bba4bc0984
José Luis Núñez <jose.nunez@educacionit.com>
2023-08-30 18:26:31 -0300

    Organiza la estructura de package

--- src/ar/digitalers/Main.java
+++ src/ar/digitalers/Main.java
@@ -1,21 +1,18 @@
-package ar.digitalers;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.UUID;
-
-import ar.digitalers.CreditCard.CreditCard;
-import ar.digitalers.RandomNum.RandomNum;
+import util.RandomNum;
+import tarjetaCredito.TarjetaCredito;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<CreditCard> list = new ArrayList<CreditCard>();
-        CreditCard creditCard = new CreditCard();
+        ArrayList<TarjetaCredito> list = new ArrayList<TarjetaCredito>();
+        TarjetaCredito creditCard = new TarjetaCredito();
 
         // Genera un ID random
-        UUID rand = UUID.randomUUID(); 
+        UUID rand = UUID.randomUUID();
 
         // Límite de dinero
         int limit = new RandomNum(100_000, 1_000_000).random();
@@ -24,7 +21,7 @@ public class Main {
         int month = new RandomNum(1, 12).random();
         int days = new RandomNum(1, 25).random();
         int years = new RandomNum(2024, 2030).random();
-        
+
         // Setea la fecha
         LocalDate localDate2 = LocalDate.of(years, month, days);
 
@@ -42,7 +39,6 @@ public class Main {
 
         // Concatena todos los números al azar en una variable de tipo String
         String codigo = code.get(0) + " " + code.get(1) + " " + code.get(2) + " " + code.get(3);
-    
 
         creditCard.setCuentabancariaID(rand);
         creditCard.setCodigo(codigo);
@@ -55,15 +51,15 @@ public class Main {
         System.out.println("MI TARJETA DE CRÉDITO");
         System.out.println();
 
-
-        // Recorremos el array de lista para obtener todos los datos de la / las tarjetas de créditos
-        for (CreditCard items: list) {
-            System.out.printf("TARJETA DE CRÉDITO\n\n* ID de la cuenta bancaria: %s\n* ID de la tarjeta: %s\n* Expira: %s\n* Máximo de retiro: $%d",
-                items.getCuentabancariaID(),
-                items.getCodigo(),
-                items.getFecha_vencimiento(),
-                items.getLimite()
-            );
+        // Recorremos el array de lista para obtener todos los datos de la / las
+        // tarjetas de créditos
+        for (TarjetaCredito items : list) {
+            System.out.printf(
+                    "TARJETA DE CRÉDITO\n\n* ID de la cuenta bancaria: %s\n* ID de la tarjeta: %s\n* Expira: %s\n* Máximo de retiro: $%d",
+                    items.getCuentabancariaID(),
+                    items.getCodigo(),
+                    items.getFecha_vencimiento(),
+                    items.getLimite());
         }
     }
 }
